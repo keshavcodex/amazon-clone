74b1248ac4cdfa34a99fce61af6303d866515b1c
keshavcodex1 <keshavcodex@gmail.com>
2022-04-07 02:36:34 +0530

    created Checkout page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route path="/" element={[<Header />, <Home />]} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -12,19 +13,61 @@ function Home() {
         />
 
         <div className="home__row">
-          {/* product */}
-          {/* product */}
+          <Product
+            id="156465"
+            title="Cup Set"
+            price={34.99}
+            image="https://5.imimg.com/data5/SELLER/Default/2021/7/YS/AX/PV/40053611/swachh-premium-tea-500x500.jpg"
+            rating={5}
+          />
+          <Product
+            id="156257465"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-          {/* product */}
-          {/* product */}
-          {/* product */}
+          <Product
+            id="4561285"
+            title="Perfume"
+            price={99}
+            image="https://m.media-amazon.com/images/I/51EW7sSIXVL._SL1100_.jpg"
+            rating={5}
+          />
+          <Product
+            id="456456"
+            title="Tooth brush"
+            price={9}
+            image="https://nypost.com/wp-content/uploads/sites/2/2022/02/Best-Electric-Toothbrushes.jpg?quality=75&strip=all"
+            rating={5}
+          />
+          <Product
+            id="54675657"
+            title="Mixer Grinder"
+            price={74.99}
+            image="https://www.ubuy.co.in/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNjFqbFJtWHJqdVMuX0FDX1NMMTEwMF8uanBn.jpg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-          {/* product */}
-          {/* product */}
+          <Product
+            id="45675271"
+            title="Mac mini"
+            price={2499}
+            image="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macmini-og-202011?wid=600&hei=315&fmt=jpeg&qlt=95&.v=1604278674000"
+            rating={5}
+          />
+          <Product
+            id="1564632575"
+            title="Samsung TV"
+            price={700}
+            image="https://m.media-amazon.com/images/I/91o08DsRplL._AC_SL1500_.jpg"
+            rating={5}
+          />
         </div>
       </div>
     </div>
